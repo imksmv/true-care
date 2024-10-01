ebb1f1d69eebfd94b218f57ce0b6c784be62317d
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2024-10-01 08:33:24 +0100

    Add server action

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,9 +1,10 @@
 "use server";
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { databases } from "../appwrite.config";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID } from "../constans";
 import { CreateAppointmentParams } from "../types/index.types";
+import { Appointment } from "../types/appwrite.types";
 
 export const createAppointment = async (
   appointment: CreateAppointmentParams,
@@ -30,6 +31,53 @@ export const getAppointment = async (appointmentId: string) => {
       appointmentId,
     );
     return appointment;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecentAppointmentsList = async () => {
+  try {
+    const appointments = await databases.listDocuments(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      [Query.orderDesc("$createdAt")],
+    );
+
+    const initialCounts = {
+      scheduledCount: 0,
+      pendingCount: 0,
+      cancelledCount: 0,
+    };
+
+    const counts = (appointments.documents as Appointment[]).reduce(
+      (acc, appointment) => {
+        switch (appointment.status) {
+          case "scheduled":
+            acc.scheduledCount += 1;
+            break;
+
+          case "pending":
+            acc.pendingCount += 1;
+            break;
+
+          default:
+            acc.cancelledCount += 1;
+            break;
+        }
+
+        return acc;
+      },
+      initialCounts,
+    );
+
+    const data = {
+      totalCounts: appointments.total,
+      ...counts,
+      documents: appointments.documents,
+    };
+
+    return data;
   } catch (error) {
     console.log(error);
   }
