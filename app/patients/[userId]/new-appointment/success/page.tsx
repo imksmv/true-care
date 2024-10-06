56647bf291966ccd82cf5990d18a6e820288a453
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2024-10-06 10:06:47 +0100

    Revert "use timezone"
    This reverts commit 251e41c1cebd15dca6586b182826820949c486af.

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -4,7 +4,7 @@ import { getAppointment } from "@/lib/actions/appointment.actions";
 import { DOCTORS } from "@/lib/constans";
 import { SearchParamProps } from "@/lib/types/index.types";
 import { SETTINGS } from "@/lib/web.config";
-import { format } from "date-fns-tz";
+import { format } from "date-fns";
 import { Calendar } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -19,13 +19,7 @@ const Success = async ({
   const doctor = DOCTORS.find(
     (doc) => doc.name === appointment?.primaryPhysician,
   );
-
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone || "UTC";
-
-  const formattedDate = format(new Date(appointment?.schedule), "PPP HH:mm", {
-    timeZone,
-  });
-
+  console.log(appointment?.schedule);
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <section className="flex items-center gap-2">
@@ -66,7 +60,7 @@ const Success = async ({
 
         <div className="flex items-center gap-2">
           <Calendar size={20} />
-          <p>{formattedDate}</p>
+          <p>{format(appointment?.schedule, "PPP HH:mm")}</p>
         </div>
       </section>
 
